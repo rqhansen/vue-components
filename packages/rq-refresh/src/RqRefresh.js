e744d191b057ee916d47e1ce88eeb23ce261b334
rq <1508477509@qq.com>
2019-03-18 13:43:05 +0800

    ''

--- packages/rq-refresh/src/RqRefresh.js
+++ packages/rq-refresh/src/RqRefresh.js
@@ -2,59 +2,61 @@ import utils from './utils'
 
 const MAXOFFSETY = 180 //最大滑动距离
 let [startY, transLateY, isTouch] = [0, 0, false]
-//获取targeteEle
-function getTargetEle() {
-    return this.options.targetEle
+
+//获取刷新图标的包裹层
+function getTargetEle(context) {
+    return context['options'].targetEle
 }
-//判断滚动元素所在的位置
-function getElePosition() {
+
+//判断滚动元素的大小和其相对于视口的位置
+function getElementBoundingClientRect(ele) {
+    if (!ele) throw new Error('element cant be null')
+    return ele.getBoundingClientRect()
+}
+
+function getElePosition(context) {
     return (
-        this.options.top -
-            getTargetEle.call(this).parentNode.getBoundingClientRect().top ||
+        context.options.top - getElementBoundingClientRect(context).top ||
         document.documentElement.scrollTop ||
         document.body.scrollTop ||
         window.pageYOffset
     )
 }
+
 //阻止和移除默认事件
 function setPreventDefault(e) {
     e.preventDefault()
 }
 
-function preventDefault() {
-    document.body.addEventListener('touchmove', setPreventDefault, {
-        passive: false
-    })
+/**
+ *
+ * @param {方法名称的字符串} methodName
+ * @param {监听的事件名称的字符串} eventName
+ * @param {监听的事件触发时调用的函数} handler
+ *
+ * eg:
+ * HandlerDefaultListener('addEventListener','touchmove',setPreventDefault);
+ * HandlerDefaultListener('removeEventListener','touchmove',setPreventDefault)
+ */
+function HandlerDefaultListener(methodName, eventName, listener) {
+    return document.body[methodName](eventName, listener, { passive: false })
 }
 
-function removePreventDefault() {
-    document.body.removeEventListener('touchmove', setPreventDefault, {
-        passive: false
-    })
-}
-
-//设置偏移
-function setTransLateY(y) {
-    getTargetEle.call(this).style.transform = `translateY(${y / 75}rem)`
+//设置偏移样式
+function setTransLateY(context, y) {
+    getTargetEle(context).style.transform = `translateY(${y / 75}rem)`
 }
 
 function touchStart(e) {
-    //初始化
-    utils.removeClass(getTargetEle.call(this).children[0], 'active')
+    let iconEle = getTargetEle(this).children[0]
+    utils.removeClass(iconEle, 'active')
     startY = e.touches[0].clientY
     transLateY = 0
 }
 
 function touchMove(e) {
+    if (getElePosition(this) > 0) return
     transLateY = parseInt(e.touches[0].clientY - startY)
-    if (getElePosition.call(this) > 0) {
-        //上拉
-        if (this.options.changeStyle) {
-            this.options.changeStyle(transLateY)
-        }
-        return
-    }
-
     /**
      * 阻止默认滑动的条件：
      * 1.第一次滑动
@@ -62,64 +64,67 @@ function touchMove(e) {
      * 3.scrollTop小于等于0
      */
     if (!isTouch && transLateY > 0) {
-        preventDefault()
+        HandlerDefaultListener(
+            'addEventListener',
+            'touchmove',
+            setPreventDefault
+        )
     }
     isTouch = true
     if (transLateY > MAXOFFSETY) transLateY = MAXOFFSETY
-    setTransLateY.call(this, transLateY)
+    setTransLateY(this, transLateY)
 }
 
 function touchEnd() {
-    if (getElePosition.call(this) > 0) return
+    if (getElePosition(this) > 0) return
     if (transLateY >= MAXOFFSETY) {
-        utils.addClass(getTargetEle.call(this).children[0], 'static')
+        utils.addClass(getTargetEle(this).children[0], 'static')
         this.options.refresh().then(() => {
-            utils.removeClass(getTargetEle.call(this).children[0], 'static')
-            utils.addClass(getTargetEle.call(this).children[0], 'active')
-            setTransLateY.call(this, 0)
+            utils.removeClass(getTargetEle(this).children[0], 'static')
+            utils.addClass(getTargetEle(this).children[0], 'active')
+            setTransLateY(this, 0)
         })
     } else {
-        setTransLateY.call(this, 0)
+        setTransLateY(this, 0)
     }
-    removePreventDefault()
+    HandlerDefaultListener(
+        'removeEventListener',
+        'touchmove',
+        setPreventDefault
+    )
     isTouch = false
 }
 /**
  * 如果有其它意外事件
  */
 function touchCancle() {
-    setTransLateY.call(this, 0)
+    setTransLateY(this, 0)
     removePreventDefault()
 }
 
 export default {
     bind: function(el, binding) {
         el.options = {}
-        // el.options.refresh = binding.value
         if (typeof binding.value === 'function') {
             el.options.refresh = binding.value
-        } else if (typeof binding.value === 'object') {
-            //expression为对象
-            let { funs, changeStyle } = binding.value
-            el.options.refresh = funs
-            el.options.changeStyle = changeStyle
         }
     },
     inserted: function(el, binding) {
-        // if (!binding.value || typeof binding.value !== 'function') return
         if (!binding.value) return
-
         let pos = getComputedStyle(el, null).position //外层容器添加类
         el.style.position = pos !== 'static' ? pos : 'relative'
         utils.addClass(el, 'rq-refresh-wrapper')
 
-        let newChild = document.createElement('div') //创建新元素
+        let newChild = document.createElement('div')
         newChild.innerHTML = `<svg class="svg-icon" aria-hidden="true"><use xlink:href="#refresh"></use></svg>`
         utils.addClass(newChild, 'refresh-icon-wrapper')
-
         el.insertBefore(newChild, el.firstChild)
+
+        //设置刷新区域的第一个元素为刷新图标
         el.options.targetEle = newChild
+        //记录初始刷新区域距离dom顶部的距离
         el.options.top = el.getBoundingClientRect().top
+
         el.addEventListener('touchstart', touchStart, { passive: false })
         el.addEventListener('touchmove', touchMove, { passive: false })
         el.addEventListener('touchend', touchEnd, { passive: false })
